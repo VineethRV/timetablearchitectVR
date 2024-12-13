e22b71b1b7c8c088a9d9ec8eb87bb3c6a743e2d9
Shreya Prasad <shreyaprasad019@gmail.com>
2024-12-13 22:09:59 +0530

    project initialised

---
+++ frontend/src/components/onboardPage/TopBar.tsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+import Logo from "/Logo.png";
+
+const Topbar = () => {
+  return (
+    <div className="flex justify-between w-full h-fit">
+      <Link to="/signin">
+        <div className="flex items-center cursor-pointer space-x-2">
+          <img className="h-8 w-8" src={Logo} alt="Logo" />
+          <h1 className="font-bold text-base">TTA</h1>
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default Topbar;
