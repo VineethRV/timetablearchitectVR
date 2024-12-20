39d3d6a41860a6a1fe5daad77a1a7b40934d4157
Shreya Prasad <shreyaprasad019@gmail.com>
2024-12-20 23:44:47 +0530

    lab update

--- frontend/src/pages/dashboard/courses/lab/labdisplay.tsx
+++ frontend/src/pages/dashboard/courses/lab/labdisplay.tsx
@@ -1,10 +1,39 @@
 "use client";
-import { Button, ConfigProvider, Input, Select } from "antd";
-import { TbTrash } from "react-icons/tb";
-import { CiExport, CiImport, CiSearch } from "react-icons/ci";
+import { Button } from "antd";
+import { CiExport, CiImport} from "react-icons/ci";
 import LabTable from "../../../../components/CoursePage/labTable";
+import Loading from "../../../../components/Loading/Loading";
+import { toast } from "sonner";
+import { statusCodes } from "../../../../types/statusCodes";
+import { BACKEND_URL } from "../../../../../config";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Lab } from "../../../../types/main";
 
 function page() {
+  const [labsData, setLabsData] = useState<Lab[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get(BACKEND_URL + "/labs",{
+        headers: {
+          authorization: localStorage.getItem("token"),
+        },
+      })
+      .then((res) => {
+        const statusCode = res.data.status;
+        if (statusCode == statusCodes.OK) {
+          console.log(res.data.message);
+        }
+        else {
+          toast.error("Server error !!")
+        }
+        setLoading(false);
+      });
+  }, []);
+
+  if(loading) return <Loading/>
   return (
     <div className="h-screen px-8 py-4 overflow-y-scroll">
       <h1 className="text-3xl font-bold text-primary mt-2">Lab Sets</h1>
@@ -18,42 +47,9 @@ function page() {
           Export
         </Button>
       </div>
-      <div className="flex space-x-8 justify-between py-4">
-        <Input
-          className="w-fit"
-          addonBefore={<CiSearch />}
-          placeholder="Course"
-        />
-
-        {/* this config to set background color of the selectors | did as specified in antd docs */}
-        <ConfigProvider
-          theme={{
-            components: {
-              Select: {
-                selectorBg: "#F3F4F6FF",
-              },
-            },
-          }}
-        >
-          <div className="flex space-x-3">
-            <Select
-              defaultValue="Sort By"
-              style={{ width: 120 }}
-              options={[]}
-            />
-            <Select defaultValue="Number of credits" options={[]} />
-            <Select defaultValue="Hours per week" options={[]} />
-          </div>
-        </ConfigProvider>
-        <div className="flex space-x-2">
-          <Button className="bg-red-500 text-white font-bold">
-            <TbTrash />Delete
-          </Button>
-          <Button>Clear filters</Button>
-        </div>
-      </div>
-
-      <LabTable />
+      <LabTable
+      setLabsData={setLabsData}
+      labData={labsData} />
 
     </div>
   );
