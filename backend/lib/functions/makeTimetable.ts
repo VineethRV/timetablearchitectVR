3d2cf6b2a1595688f49f8db6e76948bbe19133bb
VineethRV <vineethrao.cs23@rvce.edu.in>
2024-12-31 19:33:20 +0530

    huh?

--- backend/lib/functions/makeTimetable.ts
+++ backend/lib/functions/makeTimetable.ts
@@ -97,14 +97,13 @@ export async function suggestTimetable(
             //check if specified room is a department name or room name
 
             //following if statement checks if there is a specified room  (if yes, code is allowed inside)
-            if(roomsInfo&& rooms.length<i && rooms[i]!='0'){
+            if(roomsInfo&& rooms[i]!='0'){
                 console.log("\nspecific room: ",rooms[i])
                 currRoomInfo = roomsInfo.find(room => room?.name === rooms[i]);
                 //if specified room not found
                 if (!currRoomInfo) {
                     return { status: statusCodes.BAD_REQUEST, returnVal:null  };
                 }
-                
                 let feasible=scoreRooms(currRoomInfo.timetable);
                 for(let i=0;i<feasible.length;i++){
                     for(let j=0;j<feasible[i].length;j++){
