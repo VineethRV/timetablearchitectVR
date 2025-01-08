6260bf472dd5975f84f799989d92b5a5e8c1b2d8
Shreya Prasad <shreyaprasad019@gmail.com>
2025-01-08 23:32:29 +0530

    electives ui functional

--- frontend/src/components/CoursePage/electiveAddtable.tsx
+++ frontend/src/components/CoursePage/electiveAddtable.tsx
@@ -3,91 +3,94 @@ import { Button, Table, Tag, Tooltip } from 'antd';
 import type { TableProps } from 'antd';
 import { MdDelete, MdEdit } from 'react-icons/md';
 
-interface DataType {
-  key: string;
+export interface Elective {
   course: string;
-  courseCode: string;
   teachers: string[];
+  rooms: string[] | null;
 }
 
-const columns: TableProps<DataType>['columns'] = [
-  {
-    title: 'Course',
-    dataIndex: 'course',
-  },
-  {
-    title: 'Course Code',
-    dataIndex: 'courseCode',
-  },
-  {
-    title: 'Teachers',
-    dataIndex: 'teachers',
-    render: (_, { teachers }) => (
+
+const ElectiveAddTable = ({
+  electiveData,
+  setElectivesData,
+  onEditClick
+}: {
+  electiveData: Elective[];
+  setElectivesData: React.Dispatch<React.SetStateAction<Elective[]>>;
+  onEditClick: (record: Elective) => void; 
+}) => {
+
+  const handleDeleteClick = (record: Elective) => {
+    setElectivesData((prevData) => prevData.filter((item) => item.course !== record.course));
+  };
+
+
+
+  const columns: TableProps<Elective>['columns'] = [
+    {
+      title: 'Course',
+      dataIndex: 'course',
+    },
+    {
+      title: 'Teachers',
+      dataIndex: 'teachers',
+      render: (_, { teachers }) => (
         <>
-          {teachers.map((tag) => {
-            const color = 'blue'
-            return (
-              <Tag color={color} key={tag}>
-                {tag.toUpperCase()}
-              </Tag>
-            );
-          })}
+          {teachers.map((tag) => (
+            <Tag color="blue" key={tag}>
+              {tag.toUpperCase()}
+            </Tag>
+          ))}
         </>
-      )
-  },
-  {
-    title: "",
-    render: () => {
-      return (
+      ),
+    },
+    {
+      title: 'Rooms',
+      dataIndex: 'rooms',
+      render: (_, { rooms }) => (
+        <>
+          {rooms?.map((tag) => (
+            <Tag color="purple" key={tag}>
+              {tag.toUpperCase()}
+            </Tag>
+          ))}
+        </>
+      ),
+    },
+    {
+      title: '',
+      render: (record) => (
         <Tooltip title="Edit">
           <Button
             type="primary"
-           // onClick={() => handleEditClick(record.name, record.department)}
+            onClick={() => onEditClick(record)}
             shape="circle"
             icon={<MdEdit />}
           />
         </Tooltip>
-      );
+      ),
     },
-  },
-  {
-    title: "",
-    render: () => {
-      return (
+    {
+      title: '',
+      render: (record) => (
         <Tooltip title="Delete">
           <Button
             className="bg-red-400"
             type="primary"
+            onClick={() => handleDeleteClick(record)}
             shape="circle"
-            //onClick={() => handleDeleteClick(record)}
             icon={<MdDelete />}
           />
         </Tooltip>
-      );
+      ),
     },
-  }
-];
+  ];
 
-const data: DataType[] = [
-  {
-    key: '1',
-    course: 'BioSafety',
-    courseCode: 'B223AT',
-    teachers: ['Raj','Jai','Rahul'],
-  },
-  {
-    key: '2',
-    course: 'Civil',
-    courseCode: 'C223AT',
-    teachers: ['Raj','Rohan','John'],
-  },
-];
-
-const ElectiveAddTable: React.FC = () => {
-return(
-<div>
-<Table<DataType> columns={columns} dataSource={data} pagination={false}/>
-</div>
-);};
+  return (
+    <div>
+      <Table<Elective> columns={columns} dataSource={electiveData} pagination={false} />
+    </div>
+  );
+};
 
 export default ElectiveAddTable;
